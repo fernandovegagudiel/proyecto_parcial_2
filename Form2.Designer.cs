d894211bb055faaa74d633503bc383152649fa48
FERNANDO JOSE VEGA GUDIEL <fvegag1@miumg.edu.gt>
2025-05-25 17:27:26 -0600

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Minicraft
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonInicio = new Button();
+            textBoxUser = new TextBox();
+            textBoxContraseña = new TextBox();
+            btnRegistrar = new Button();
+            SuspendLayout();
+            // 
+            // buttonInicio
+            // 
+            buttonInicio.Location = new Point(252, 219);
+            buttonInicio.Name = "buttonInicio";
+            buttonInicio.Size = new Size(94, 29);
+            buttonInicio.TabIndex = 0;
+            buttonInicio.Text = "Inicio Sesion";
+            buttonInicio.UseVisualStyleBackColor = true;
+            buttonInicio.Click += buttonInicio_Click;
+            // 
+            // textBoxUser
+            // 
+            textBoxUser.Location = new Point(201, 118);
+            textBoxUser.Name = "textBoxUser";
+            textBoxUser.Size = new Size(326, 27);
+            textBoxUser.TabIndex = 1;
+            // 
+            // textBoxContraseña
+            // 
+            textBoxContraseña.Location = new Point(201, 162);
+            textBoxContraseña.Name = "textBoxContraseña";
+            textBoxContraseña.PasswordChar = '*';
+            textBoxContraseña.Size = new Size(326, 27);
+            textBoxContraseña.TabIndex = 2;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(405, 219);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(94, 29);
+            btnRegistrar.TabIndex = 3;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.minecraft_world_scenery_desktop_wallpaper_preview;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRegistrar);
+            Controls.Add(textBoxContraseña);
+            Controls.Add(textBoxUser);
+            Controls.Add(buttonInicio);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonInicio;
+        private TextBox textBoxUser;
+        private TextBox textBoxContraseña;
+        private Button button1;
+        private Button btnRegistrar;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minicraft
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInicio_Click(object sender, EventArgs e)
+        {
+            string nombre = textBoxUser.Text.Trim();
+            string contraseña = textBoxContraseña.Text.Trim();
+
+            bool encontrado = false;
+
+            if (File.Exists("usuarios.txt"))
+            {
+                foreach (string linea in File.ReadAllLines("usuarios.txt"))
+                {
+                    string[] datos = linea.Split(',');
+
+                    if (datos.Length == 2 && datos[0] == nombre && datos[1] == contraseña)
+                    {
+                        encontrado = true;
+                        break;
+                    }
+                }
+            }
+
+            if (encontrado)
+            {
+                Form1 form = new Form1();
+                form.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta", "Error de acceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxContraseña.Clear();
+                textBoxUser.Focus();
+            }
+        }
+
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string nuevoUsuario = textBoxUser.Text;
+            string nuevaContrasena = textBoxContraseña.Text;
+
+          
+            if (string.IsNullOrEmpty(nuevoUsuario) || string.IsNullOrEmpty(nuevaContrasena))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre de usuario y una contraseña.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string nuevoRegistro = $"{nuevoUsuario},{nuevaContrasena}\n";
+            File.AppendAllText("usuarios.txt", nuevoRegistro);
+
+            MessageBox.Show("Usuario registrado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            textBoxUser.Clear();
+            textBoxContraseña.Clear();
+        }
+
+    }
+
+}
+
